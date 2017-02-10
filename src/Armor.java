3fe1a41c69629dcb6d3247aba4dccb819cbbc341
Trav987 <tcooper3@albany.edu>
2017-02-10 15:40:02 -0500

    Food and armor class added

---
+++ src/Armor.java
@@ -0,0 +1,19 @@
+public class Armor
+{
+	public int getDefense()
+	{
+		return defense;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public int getWeight()
+	{
+		return weight;
+	}
+	public void examine()
+	{
+		System.out.println();
+	}
+}
