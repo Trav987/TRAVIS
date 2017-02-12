b72a0058b6f64e1d2c83b3527d8e7be7ed6d7d3a
Trav987 <tcooper3@albany.edu>
2017-02-12 12:35:24 -0500

    Main method that compiles the programs all together

--- src/Game.java
+++ src/Game.java
@@ -4,18 +4,35 @@ public class Game
 	{
 		Item item = new Item();
 		item.setName();
+		item.setWeight();
 		item.examine();
 		
 		System.out.println();
 		System.out.println();
 		
 		Weapon weapon = new Weapon();
+		weapon.setName();
+		weapon.setWeight();
+		weapon.setDamage();
 		weapon.examine();
 		
 		System.out.println();
 		System.out.println();
 		
 		Armor armor = new Armor();
+		armor.setDefense();
+		armor.setName();
+		armor.setWeight();
 		armor.examine();
+		
+		System.out.println();
+		System.out.println();
+		
+		Food food = new Food();
+		food.setName();
+		food.setName();
+		food.setNutrition();
+		food.setQuantity(0);
+		food.examine();
 	}
 }
