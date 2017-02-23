c1405fe28cb8fde163fc900087f5226c400f62fe
Trav987 <tcooper3@albany.edu>
2017-02-22 20:58:43 -0500

    added the error/2

--- src/Recursive.java
+++ src/Recursive.java
@@ -1,15 +1,28 @@
 
 public class Recursive 
 {
-	public static double recursive(double x)
-	{
-		if(x==0)
-			return 1;
-		return 0.5*(recursive(x-1)+ x/recursive(x-1));
 	
+	public static double recursive(double x)
+	{ 
+		
+		if (x==0)
+			 return 1;
+		return 0.5*(recursive(x-1) + x/recursive(x-1));
+		//new
+		
+	 
+		
 	}
+	
 	public static void main (String [] args)
 	{
-		System.out.print(recursive(25));
+		//Start
+		double error = 0;
+		error = error/2;
+		error = recursive(30);
+		
+		System.out.print(error);
+		
+		
 	}
 }
