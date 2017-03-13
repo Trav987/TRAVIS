8a5b364f8ad9f12d89cee8a2197f695e7e385793
Trav987 <tcooper3@albany.edu>
2017-03-12 22:08:32 -0400

    Fix the main method

--- src/second/main.java
+++ src/second/main.java
@@ -5,9 +5,11 @@ public class main
 	public static void main(String[] args)
 	{
 		link d = new link();
-		d.addAtEnd("Z ");
-		d.addAtEnd("X ");
+		String tmp;
 		d.addAtStart("A");
-		
+		for(char alphabet = 'A'; alphabet <= 'Z';alphabet++) {
+		    System.out.println(alphabet);
+		}
+		for(int i = 0;i < d.length;i++)
 	}
 }
