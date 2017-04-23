3eac9f51bf09d4bd3b84a4847e312e0214a46b68
Trav987 <tcooper3@albany.edu>
2017-04-23 12:52:15 -0400

    fix bug for the unsorted list

--- src/HW5/Bubble.java
+++ src/HW5/Bubble.java
@@ -1,10 +1,26 @@
+package HW5;
+
 import java.lang.reflect.Array;
 
 public class Bubble 
 {
-	public void sortLoop(int x[])
+	 public static void main(String[] args) 
+	 {
+		 int[] array = {8,4,-2,5,6,10,120};
+	        
+	        System.out.println("The Unsorted List:");
+	        for(int i = 0; i < array.length; i++)
+	        {
+	            System.out.print(array[i]);
+	            if(i != (array.length-1))
+	                System.out.print(", ");
+	        }
+	 }
+	public void sortLoop(int[] x)
 	{
-		for(int i = 0; i < x[i].length() - 2; i++)
+		
+		 
+		for(int i = 0; i < x.length - 2; i++)
 		{
 			if (x[i] > x[i+1])
 			{
